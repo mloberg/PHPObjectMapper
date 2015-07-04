5d0581c2f09262d6fe521af95e866d39da061190
Matthew Loberg <loberg.matt@gmail.com>
2015-07-04 10:51:03 -0500

    Add tests for the array mapper

--- tests/ObjectMapperTest.php
+++ tests/ObjectMapperTest.php
@@ -58,4 +58,82 @@ class ObjectMapperTest extends \PHPUnit_Framework_TestCase
         // Assert things aren't automatically mapped
         $this->assertNull($employee->getAddress()->getCountry());
     }
+
+    /**
+     * Test basic array mapping works
+     */
+    public function testArrayMapping()
+    {
+        $person = [
+            'firstName' => 'John',
+            'lastName'  => 'Doe',
+            'contact'   => [
+                'email'    => 'jdoe@example.com',
+                'phone'    => '555-555-5555',
+                'address1' => '555 Main St',
+                'city'     => 'Minneapolis',
+                'state'    => 'MN',
+                'zipCode'  => '55555',
+            ],
+        ];
+
+        $mapper = new ObjectMapper();
+
+        /** @var Mock\Employee $employee */
+        $employee = $mapper->map($person, 'Mlo\ObjectMapper\Tests\Mock\Employee');
+
+        $this->assertInstanceOf('Mlo\ObjectMapper\Tests\Mock\Employee', $employee);
+        $this->assertInstanceOf('Mlo\ObjectMapper\Tests\Mock\Address', $employee->getAddress());
+
+        // Assert multiple mappings work as expected
+        $this->assertEquals($person['firstName'], $employee->getFirstName());
+
+        // Assert properties with same names map and can map by properties
+        $this->assertEquals($person['lastName'], $employee->getLastName());
+
+        // Assert mapping from a dot-notation path works
+        $this->assertEquals($person['contact']['email'], $employee->getEmail());
+
+        // Assert things aren't automatically mapped
+        $this->assertNull($employee->getAddress()->getCountry());
+    }
+
+    /**
+     * Test that objects within arrays can be mapped
+     */
+    public function testArrayObjectMapping()
+    {
+        $person = [
+            'firstName' => 'John',
+            'lastName'  => 'Doe',
+        ];
+
+        $person['contact'] = (new Contact())
+            ->setEmail('jdoe@example.com')
+            ->setPhone('555-555-5555')
+            ->setAddress1('555 Main St')
+            ->setCity('Minneapolis')
+            ->setState('MN')
+            ->setZipCode(55555);
+
+        $mapper = new ObjectMapper();
+
+        /** @var Mock\Employee $employee */
+        $employee = $mapper->map($person, 'Mlo\ObjectMapper\Tests\Mock\Employee');
+
+        $this->assertInstanceOf('Mlo\ObjectMapper\Tests\Mock\Employee', $employee);
+        $this->assertInstanceOf('Mlo\ObjectMapper\Tests\Mock\Address', $employee->getAddress());
+
+        // Assert multiple mappings work as expected
+        $this->assertEquals($person['firstName'], $employee->getFirstName());
+
+        // Assert properties with same names map and can map by properties
+        $this->assertEquals($person['lastName'], $employee->getLastName());
+
+        // Assert mapping from a dot-notation path works
+        $this->assertEquals($person['contact']->getEmail(), $employee->getEmail());
+
+        // Assert things aren't automatically mapped
+        $this->assertNull($employee->getAddress()->getCountry());
+    }
 }
